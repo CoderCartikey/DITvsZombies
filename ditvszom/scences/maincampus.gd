ac0044b864f55909cd4ea26ae38eb4f1bb7f0b92
Kartikey Bhardwaj <kartikey123del@gmail.com>
2026-08-10 13:36:30 +0530

    Update maincampus.gd

--- ditvszom/scences/maincampus.gd
+++ ditvszom/scences/maincampus.gd
@@ -4,14 +4,20 @@ extends Node2D
 @onready var player = $player
 
 func _ready():
-	# Saare zombies fix karo
+	# Player collision fix
+	player.set_collision_layer_value(1, false)
+	player.set_collision_layer_value(2, true)
+	player.set_collision_mask_value(1, true)
+	player.set_collision_mask_value(3, true)
+	
+	# Saare zombies fix
 	var zom_node = get_node("ZOM")
 	for zombie in zom_node.get_children():
 		zombie.add_to_group("zombies")
-		# Collision layers fix
 		zombie.set_collision_layer_value(1, false)
 		zombie.set_collision_layer_value(3, true)
 		zombie.set_collision_mask_value(1, true)
+		zombie.set_collision_mask_value(2, true)
 
 func _process(delta):
 	camera.global_position = player.global_position
