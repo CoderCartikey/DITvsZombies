334906a329a0f76404d186a9704867240dbdb59a
Kartikey Bhardwaj <kartikey123del@gmail.com>
2026-08-10 12:54:21 +0530

    Update infirmary.gd

--- ditvszom/scences/infirmary.gd
+++ ditvszom/scences/infirmary.gd
@@ -1,6 +1,11 @@
 extends Area2D
 
+func _ready():
+	print("Infirmary ready!")
+	body_entered.connect(_on_body_entered)
+
 func _on_body_entered(body):
+	print("Something entered infirmary: ", body.name)
 	if body.name == "player":
 		body.health = 100
 		var hb = body.get_node_or_null("/root/maincampus/CanvasLayer/Control/HealthBar")
