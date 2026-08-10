a14eee2a429a4309d11f61d576376ea482fa8300
Kartikey Bhardwaj <kartikey123del@gmail.com>
2026-08-10 13:40:55 +0530

    Update bullet.gd

--- ditvszom/bullet.gd
+++ ditvszom/bullet.gd
@@ -4,9 +4,8 @@ const SPEED = 600.0
 var direction = Vector2.ZERO
 
 func _ready():
-	# Bullet sirf layer 3 (zombies) ko detect kare
 	set_collision_layer_value(1, false)
-	set_collision_layer_value(2, true)
+	set_collision_layer_value(4, true)
 	set_collision_mask_value(1, false)
 	set_collision_mask_value(3, true)
 	body_entered.connect(_on_body_entered)
