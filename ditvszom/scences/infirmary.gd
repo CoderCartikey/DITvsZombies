c9998a54747c3354520bc6290a72b925a4df414b
Kartikey Bhardwaj <kartikey123del@gmail.com>
2026-08-10 13:03:03 +0530

    infirmary

--- ditvszom/scences/infirmary.gd
+++ ditvszom/scences/infirmary.gd
@@ -5,7 +5,7 @@ func _ready():
 	body_entered.connect(_on_body_entered)
 
 func _on_body_entered(body):
-	print("Something entered infirmary: ", body.name)
+	print("Entered: ", body.name, " | Class: ", body.get_class())
 	if body.name == "player":
 		body.health = 100
 		var hb = body.get_node_or_null("/root/maincampus/CanvasLayer/Control/HealthBar")
