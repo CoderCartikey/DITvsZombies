f0e69abf898d486f166ebf80fb0ea6f14e26400a
Kartikey Bhardwaj <kartikey123del@gmail.com>
2026-08-12 21:48:09 +0530

    new node Area2d named zoomarea

---
+++ ditvszom/scences/zoom_area.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+@onready var camera = $"../Camera2D"
+
+func _on_body_entered(body):
+	if body.name == "player":
+		var tween = create_tween()
+		tween.tween_property(camera, "zoom", Vector2(2, 2), 0.5)
+		
+func _on_body_exited(body):
+	if body.name == "player":
+		var tween = create_tween()
+		tween.tween_property(camera, "zoom", Vector2(1, 1), 0.5)
+		
