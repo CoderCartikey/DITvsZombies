02c0451c021a414e1fc37d72c5954a264148625b
Kartikey Bhardwaj <kartikey123del@gmail.com>
2026-08-12 22:11:43 +0530

    Update zoom_area.gd

--- ditvszom/scences/zoom_area.gd
+++ ditvszom/scences/zoom_area.gd
@@ -2,5 +2,8 @@ extends Area2D
 
 @onready var camera = $"../player/Camera2D"
 
-func _on_body_entered(body):
-	print("BODY ENTERED:", body.name)
+
+func _on_zoom_area_body_entered(body: Node2D) -> void:
+	if body.name == "player":
+		print("ZOOM TRIGGERED")
+		camera.zoom = Vector2(2, 2)
