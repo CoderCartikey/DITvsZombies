f6a5e21771f864979d0bd4a98bfcdd8469e964f0
Kartikey Bhardwaj <kartikey123del@gmail.com>
2026-08-12 22:07:22 +0530

    1

--- ditvszom/scences/zoom_area.gd
+++ ditvszom/scences/zoom_area.gd
@@ -3,6 +3,4 @@ extends Area2D
 @onready var camera = $"../player/Camera2D"
 
 func _on_body_entered(body):
-	if body.name == "player":
-		camera.zoom = Vector2(2, 2)
-		print("ZOOM TRIGGERED")
+	print("BODY ENTERED:", body.name)
