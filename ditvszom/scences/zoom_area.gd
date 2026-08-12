94d1f8130e3c9113c751e7862fce7d1a8df34802
Kartikey Bhardwaj <kartikey123del@gmail.com>
2026-08-12 21:55:26 +0530

    Update zoom_area.gd

--- ditvszom/scences/zoom_area.gd
+++ ditvszom/scences/zoom_area.gd
@@ -4,11 +4,5 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body.name == "player":
-		var tween = create_tween()
-		tween.tween_property(camera, "zoom", Vector2(2, 2), 0.5)
-		
-func _on_body_exited(body):
-	if body.name == "player":
-		var tween = create_tween()
-		tween.tween_property(camera, "zoom", Vector2(1, 1), 0.5)
-		
+		camera.zoom = Vector2(2, 2)
+		print("ZOOM TRIGGERED")
